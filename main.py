9c797802aa060e00b9530e77ccf7d7b207671213
Austin Suarez <austinmsuarez@gmail.com>
2020-08-23 11:42:46 -0700

    busy status w/ message test

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import unicornhathd
-
+import time
+from PIL import Image, ImageDraw, ImageFont
 
 def busy_status():
-    print(""" I am currently busy
 
-    Please wait until this turns green to make contact.
-
-    Thank you.
-    """)
+    text = " I am currently available Feel free to enter. :) "
 
     unicornhathd.clear()
     unicornhathd.brightness(.6)
@@ -20,15 +17,14 @@ def busy_status():
     try:
         while True:
             unicornhathd.show()
-
+            time.sleep(.02)
+            print_status(text, 255, 0, 0)
     except KeyboardInterrupt:
         print("error")
         unicornhathd.off()
 
 def available_status():
-     print(""" I am currently available
-    Feel free to enter. :) """)
-
+    
     unicornhathd.clear()
     unicornhathd.brightness(.6)
 
@@ -43,3 +39,42 @@ def available_status():
     except KeyboardInterrupt:
         print("error")
         unicornhathd.off()
+
+
+def print_status(myStatusText, r, g, b):
+    TEXT = myStatusText
+
+    FONT = ('/usr/share/fonts/true/type/freefont/FreeSansBold.ttf'.12)
+
+    width, height = unicornhathd.get_shape()
+
+    unicornhathd.rotation(90)
+    unicornhathd.brightness(.6)
+
+    text_x = 1
+    text_y = 2
+    
+    font_file, font_size = FONT
+
+    font = ImageFont.truetype(font_file. font_size)
+    text_widt, text_height = font.getsize(TEXT)
+
+    text_width += width + text_x
+
+    image = Image.new('RGB', (text_width, max(height, text_height)). (0,0,0))
+
+    draw = ImageDraw.Draw(image)
+
+    for scroll in range(text_width - width):
+        for x in range(width):
+            for y in range(height):
+                pixel = image.getpixel((x + scroll, y))
+
+                unicornhathd.set_pixel(width-1-x, y,r,g,b)
+
+        unicornhathd.show()
+
+        time.sleep(0.02)
+
+    
+busy_status()
